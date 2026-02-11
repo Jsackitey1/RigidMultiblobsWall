8d41e464d7de9b6514a85a741dd227f1219e3a49
Florencio Balboa Usabiaga <fbusabiaga@gmail.com>
2026-02-11 12:25:32 +0100

    test double layer works with eta != 1

--- mobility/test_double_layer_finite.py
+++ mobility/test_double_layer_finite.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
   N_src = 1
   N_trg = 1
   N = N_src + N_trg
-  eta = 1
+  eta = 1000
   a = 1
-  z_min = 2
+  z_min = 1
   dx = 1e-06
   wall = 0
 
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     velocity_finite_nm = -mob.no_wall_mobility_trans_times_force_source_target_numba(r_src - normals_src * 0.5 * dx, r_trg, vector_src, radius_source, radius_target, eta)
     velocity_finite_vp = -mob.no_wall_mobility_trans_times_force_source_target_numba(r_src + vector_src * 0.5 * dx, r_trg, normals_src, radius_source, radius_target, eta) 
     velocity_finite_vm = -mob.no_wall_mobility_trans_times_force_source_target_numba(r_src - vector_src * 0.5 * dx, r_trg, normals_src, radius_source, radius_target, eta)
-    vn = -np.einsum('bi,bi->b', vector_src, normals_src) / eta 
+    vn = -np.einsum('bi,bi->b', vector_src, normals_src) 
     velocity_pressure = np.zeros((N_src, 3))
     x = np.zeros((N_src, 3))
     x[:,0] = 1
@@ -82,7 +82,7 @@ if __name__ == '__main__':
     x[:,:] = 0    
     x[:,2] = 1
     velocity_pressure[:,2] = mob.no_wall_pressure_Stokeslet_numba(r_src, r_trg, x) * vn 
-    velocity_finite = (velocity_finite_np - velocity_finite_nm + velocity_finite_vp - velocity_finite_vm) / dx + velocity_pressure    
+    velocity_finite = (velocity_finite_np - velocity_finite_nm + velocity_finite_vp - velocity_finite_vm) * (eta / dx) + velocity_pressure 
     
     vel_diff = velocity_finite - velocity_numba
     print('No wall test')
